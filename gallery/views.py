5542732e5218fe5489b71a3f46d0966b4f901604
kebasoh <ongatikebasoba@gmail.com>
2020-01-31 11:24:05 +0300

    added the templates

--- gallery/views.py
+++ gallery/views.py
@@ -10,45 +10,20 @@ def welcome(request):
 def gallery_of_day(request):
     date = dt.date.today()
 
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'gallery/today-gallery.html', {"date": date,})
 
-def convert_dates(dates):
+def past_days_gallery(request, past_date):
     
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-
-def past_days_gallery(request,past_date):
-    # Converts data from the string Url
     try:
         # Converts data from the string Url
-       date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
-  
+        assert False
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(gallery_of_day)
+
+    return render(request, 'gallery/past-gallery.html', {"date": date})
