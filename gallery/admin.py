9fade399eecabdd63ca7b050d097b641baed5a0b
kebasoh <ongatikebasoba@gmail.com>
2020-01-31 20:59:07 +0300

    added the test.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Editor,images
+from .models import Editor,images,tags
 
-
+class ImagesAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
 
 admin.site.register(Editor)
-admin.site.register(images)
+admin.site.register(images,ImagesAdmin)
+admin.site.register(tags)
--- gallery/tests.py
+++ gallery/tests.py
@@ -20,30 +20,24 @@ class EditorTestClass(TestCase):
     def test_save_method(self):
         self.Steve.save_editor()
         editors = Editor.objects.all()
-        self.assertTrue(len(editors) > 0)       
-
-class imagesTestClass(TestCase):
+        self.assertTrue(len(editors) > 0) 
+              
+class ImagesTestClass(TestCase):
     
     def setUp(self):
         # Creating a new editor and saving it
-        self.Steve= Editor(first_name = 'Steve', last_name ='Kebaso', email ='ongatikebaso@gamil.com')
+        self.Steve= Editor(first_name = 'Steve', last_name ='Kebaso', email ='ongatikebaso@gmail.com')
         self.Steve.save_editor()
 
-        self.new_image= images(title = 'Test images',post = 'This is a random test Post',editor = self.Steve)
-        self.new_image.save()
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
 
-        
+        self.new_images= Images(title = 'Test Images',post = 'This is a random test Post',editor = self.Steve)
+        self.new_images.save()
+
+        self.new_images.tags.add(self.new_tag)
 
     def tearDown(self):
         Editor.objects.all().delete()
-        images.objects.all().delete() 
-                
-    def test_get_gallery_today(self):
-        today_gallery = images.todays_gallery()
-        self.assertTrue(len(today_gallery)>0)  
-      
-    def test_get_gallery_by_date(self):
-        test_date = '2017-03-17'
-        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
-        gallery_by_date = Image.days_gallery(date)
-        self.assertTrue(len(gallery_by_date) == 0)        
+        tags.objects.all().delete()       
